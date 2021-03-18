6fe1ce9c1d02141d9be55980c90aabbd1702e446
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-03-18 05:56:09 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: ['babel-polyfill', './client/index.js'],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  context: __dirname,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
